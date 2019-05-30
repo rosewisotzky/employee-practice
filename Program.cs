7fe30b5b9394780a99d7845df9642842a13928c7
Rose Wisotzky <rose@rosewisotzky.com>
2019-05-30 14:13:25 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace employees_practice_classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
